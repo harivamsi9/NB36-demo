2e919592ada245b068fc220a4bc95ee149ca50e0
harivamsi9 <harivamsiyadavalli@gmail.com>
2024-09-28 23:58:38 -0700

    added taktile API3 logic, MVP done

--- .github/workflows/taktile_github_integration/taktile_update_handler.py
+++ .github/workflows/taktile_github_integration/taktile_update_handler.py
@@ -31,25 +31,65 @@ def api_not_ok_error(response):
     print(f"Request failed with status code {response.status_code}: {response.text}")
     return 
     
+def file_exists(file_path):
+    """ Returns True if file_name.py present in dir_path else False """
+    return os.path.isfile(file_path)
+
+def modify_codeNode_with_src_code(flow_id, node_id, src_code):
+    headers = auth_header()
+    data = {
+        "data": {
+        "flow_id": f"{flow_id}",
+        "node_id": f"{node_id}",
+        "src_code": f"{src_code}"
+        },
+        "metadata": {
+            "version": "v1.0",
+            "entity_id": "string"
+        },
+        "control": {
+            "execution_mode": "sync"
+        }
+    }
+
+    response_srcCode = requests.post(url, headers=headers, data=json.dumps(data))
+    if response_srcCode.status_code == 200:
+        return response_srcCode["data"]["message"]
+    
+    else: api_not_ok_error(response_srcCode)
+        
+
+
+
+    
 
 def extract_codeNode_and_update_srcCode(flow_id, res):
-    # code_nodes = [
-    #     {
-    #         "node_name": node["node_name"],
-    #         "node_id": node["node_id"]
-    #     }
-    #     for node in res["data"]["graph"] if "node_type" in node and node["node_type"] == "code_node"
-    # ]
+    decision_flow_lookup = os.path.join(os.path.dirname(__file__), '..', "..", ",,", 'decision_flow.json')["decision_flow_ids"]
+
+    dir_path = decision_flow_lookup[flow_id]
+
     for node in res["data"]["graph"]:
+
         if "node_type" in node and node["node_type"] == "code_node":
             # Found CODE_NODE
+            node_id = node["node_id"]
+            print(f'flow_id: {flow_id}, dirPath: {dir_path}, Node Name: { node["node_name"] }, Node ID: { node_id }')
+
             # UPDATE SRC CODE IF FILE EXISTS
-            print(f'flow_id: {flow_id}, Node Name: { node["node_name"] }, Node ID: { node["node_id"] }')
+            file_path = os.path.join(dir_path, node["node_name"] + '.py')
+            if file_exists(file_path):
+                src_code = ""
+                # modify the src_code in the code_node
+                with open(file_path, 'r') as file:
+                        src_code = file.read()
 
-            # decision_flow_lookup = os.path.join(os.path.dirname(__file__), '..', "..", ",,", 'data.json')
+                modify_codeNode_with_src_code(flow_id, node_id, src_code)
 
 
-            # flow_dir = 
+
+
+
+
 
 
 
@@ -61,16 +101,11 @@ def extract_codeNode_and_update_srcCode(flow_id, res):
 
 
 if __name__  == "__main__":
-
-    print("OS CURR PATH:", os.path.dirname(__file__) )
-
-    # Define the API endpoint and headers
     # ENDPOINT: Return a list of Decision Flows in a customer’s workspace
     url = 'https://eu-central-1.taktile-org.decide.taktile.com/run/api/v1/flows/list-decision-graphs/sandbox/decide'
 
     headers = auth_header(Taktile_API_KEY)
     
-
     data = {
         "data": {
             "organization_name": f"{ORGANIZATION_NAME}"
@@ -105,13 +140,6 @@ if __name__  == "__main__":
         if response_2.status_code == 200:
             print(f"Request_2 for flow_id: {flow_id} was successful:")
             extract_codeNode_and_update_srcCode(flow_id, response_2.json())
-
-
-            # Print the extracted code nodes
-            # for code_node in code_nodes:
-                # this is where update_src_code
-                # print(f"flow_id: {flow_id}, Node Name: {code_node['node_name']}, Node ID: {code_node['node_id']}")
-
         else:
             api_not_ok_error(response)
 
